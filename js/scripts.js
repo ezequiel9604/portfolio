d4dae096688c41fe91f5f12cc49c755809aa4ad9
ezequiel <ezequiel9604@gmail.com>
2022-07-10 07:31:01 +0800

    ./index.html was modified, html structure of (projects) section was included... ./scss/main.scss was modified, scss code for (projects) section was added... done

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+window.addEventListener("load", function(){
+
+    const projectsSlideshow = document.getElementById("projects__slideshow");
+
+    const projectButton1 = document.getElementById("projects__control_1");
+    const projectButton2 = document.getElementById("projects__control_2");
+    const projectButton3 = document.getElementById("projects__control_3");
+    
+
+    projectButton1.addEventListener("click", function(ev){
+        if(projectsSlideshow.scrollLeft > 0){
+            projectsSlideshow.scrollTo(0, 0);
+        }
+    });
+
+    projectButton2.addEventListener("click", function(ev){
+        if(projectsSlideshow.scrollLeft == 0 || projectsSlideshow.scrollLeft > 0){
+            projectsSlideshow.scrollTo((projectsSlideshow.offsetWidth+1), 0);
+        }
+    });
+
+    projectButton3.addEventListener("click", function(ev){
+        if(projectsSlideshow.scrollLeft == 0 || projectsSlideshow.scrollLeft > 0){
+            projectsSlideshow.scrollTo(projectsSlideshow.offsetWidth*2, 0);
+        }
+    });
+
+});
